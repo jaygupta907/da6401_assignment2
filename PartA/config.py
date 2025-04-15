bf042c216ea56f894c725ede47a630a982fbd12b
jaygupta907 <ae21b026@smail.iitm.ac.in>
2025-04-15 05:28:01 -0700

    changed sweep

--- PartA/config.py
+++ PartA/config.py
@@ -12,7 +12,7 @@ def get_args():
     # Adding various arguments for training configuration
     parser.add_argument('--wandb_project', type=str, default='Convolution Neural Networks', help='Project Name')
     parser.add_argument('--wandb_entity', type=str, default='jay_gupta-indian-institute-of-technology-madras', help='WandB entity name')
-    parser.add_argument('--num_epochs', type=int, default=20, help='Number of epochs')
+    parser.add_argument('--num_epochs', type=int, default=30, help='Number of epochs')
     parser.add_argument('--batch_size', type=int, default=64, help='Batch size')
     parser.add_argument('--apply_augmentation', type=bool, default=True, help='Whether to apply augmentation or not')
     parser.add_argument('--apply_batch_norm',type=bool,default=True,help='Whether to use the batch normalization layer')
